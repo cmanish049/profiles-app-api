6bca00c28ca619240a3a8b089fa2f0aadedf271b
cmanish049 <cmanish049@gmail.com>
2022-01-30 15:13:41 +0545

    API View

---
+++ profiles_api/serializers.py
@@ -0,0 +1,6 @@
+import imp
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """Serializes a name field for testing our APIView"""
+    name = serializers.CharField(max_length=10)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import imp
+from urllib import response
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from . import serializers
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        return Response({'message': 'Hello', 'data': {'name': 'John', 'age': 39}})
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)

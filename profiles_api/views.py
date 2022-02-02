261849b1c16ebcd4e6d3b68c563afe9725a6d273
cmanish049 <cmanish049@gmail.com>
2022-02-02 18:40:21 +0545

    added profiles viewset and permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,13 @@
-import imp
-from urllib import response
+from os import name
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
-from . import serializers
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -22,3 +25,36 @@ class HelloApiView(APIView):
             return Response({'message': message})
         else:
             return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+        return Response({'from': 'ViewSet', 'message': 'Hello', 'data': {'name': 'John', 'age': 39}})
+
+    
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status.HTTP_400_BAD_REQUEST
+            )
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
